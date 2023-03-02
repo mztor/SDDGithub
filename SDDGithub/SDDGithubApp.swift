9d4ce250c10f6d8a0653db00bfc5e95a0d025c95
mztor <stor@macarthur.nsw.edu.au>
2023-03-03 09:09:21 +1100

    Initial Commit

---
+++ SDDGithub/SDDGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDDGithubApp.swift
+//  SDDGithub
+//
+//  Created by Sarah Tor on 3/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct SDDGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
